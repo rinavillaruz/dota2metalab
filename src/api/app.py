b46aea53bf496a181f226b472b5f4b140d0d0177
Rina Villaruz <142535552+rinavillaruz@users.noreply.github.com>
2026-04-28 12:20:47 +0200

    chore: trigger initial api build

--- src/api/app.py
+++ src/api/app.py
@@ -66,4 +66,4 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)# trigger

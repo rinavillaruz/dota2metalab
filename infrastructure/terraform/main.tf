cfefa4f35e8d5045cd7a4d4756f899d3de665000
Rina Villaruz <142535552+rinavillaruz@users.noreply.github.com>
2026-04-28 18:29:33 +0200

    fix: remove pre-install hooks from PVC and StatefulSet

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -92,3 +92,40 @@ resource "helm_release" "argocd" {
 
   depends_on = [module.eks]
 }
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:aud" = "sts.amazonaws.com"
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+
+  depends_on = [
+    module.eks,
+    aws_iam_role_policy_attachment.ebs_csi_driver
+  ]
+}

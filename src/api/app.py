5f672c541950b90ec263d1029ff9677ec7c117d4
Rina Villaruz <142535552+rinavillaruz@users.noreply.github.com>
2026-05-03 12:11:02 +0200

    ci: trigger build-api

--- src/api/app.py
+++ src/api/app.py
@@ -66,4 +66,5 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)# trigger
+    app.run(host='0.0.0.0', port=port, debug=True)
+# trigger
